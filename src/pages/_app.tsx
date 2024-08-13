bd99596922f7ee7e5339b2eeda41c40ec0398b3e
mgd1984 <alecjohnmiller@gmail.com>
2024-08-13 16:57:34 -0400

    chore: Update Auth0Provider configuration in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -63,8 +63,8 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || 'YOUR_DEFAULT_DOMAIN'}
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || 'YOUR_DEFAULT_CLIENT_ID'}
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
     >
       <Component
         {...pageProps}
