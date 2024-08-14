63d82c72c17aa737822f4885ddb30d04cda1e8be
mgd1984 <alecjohnmiller@gmail.com>
2024-08-13 20:48:19 -0400

    chore: Update Auth0Provider configuration and remove unnecessary props in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -61,18 +61,16 @@ const App = ({ Component, pageProps }: AppProps) => {
     });
   };
 
+  const redirectUri = typeof window !== 'undefined' ? window.location.origin : '';
+
+
   return (
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
+      authorizationParams={{ redirect_uri: redirectUri }}
     >
-      <Component
-        {...pageProps}
-        loggedIn={loggedIn}
-        user={user}
-        login={login}
-        logout={logout}
-      />
+      <Component {...pageProps} />
     </Auth0Provider>
   );
 
